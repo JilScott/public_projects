38b081ac0daec104b243dfe4caba747ba6ff51be
Jilian Scott <jilscott4@gmail.com>
2014-04-25 14:32:14 -0500

    first commit

---
+++ NewProj/NewProj/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NewProj
+//
+//  Created by Jackie Scott on 4/25/14.
+//  Copyright (c) 2014 Jilian Scott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
